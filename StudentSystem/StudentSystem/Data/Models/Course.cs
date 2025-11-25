7cfd07b9a63a24c32d5677344ffc5da8f511625a
Yoan-Topchiyski <yoantopchiyski@gmail.com>
2025-11-25 13:25:45 +0200

    Small refactoring.

--- StudentSystem/StudentSystem/Data/Models/Course.cs
+++ StudentSystem/StudentSystem/Data/Models/Course.cs
@@ -2,7 +2,7 @@ namespace StudentSystem.Data.Models
 {
     public class Course
     {
-        public int Id { get; set; }
+        public int CourseId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
