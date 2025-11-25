70f4027bbd1648f1f8b38f2714dc7b70aec5740f
nikonikolov1001 <sioneni28@gmail.com>
2025-11-25 14:13:57 +0200

    Changes  dbcontext

--- StudentSystem/StudentSystem/Data/StudentSystemContext.cs
+++ StudentSystem/StudentSystem/Data/StudentSystemContext.cs
@@ -1,10 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using StudentSystem.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SchoolDB
 {
@@ -12,25 +7,40 @@ namespace SchoolDB
     {
         public SchoolDbContext()
         {
-
         }
 
         public SchoolDbContext(DbContextOptions<SchoolDbContext> options)
-           : base(options)
+            : base(options)
         {
-
         }
 
-
+      
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=.;Database=StudentSystemDB;Trusted_Connection=True;Encrypt=False;");
+            }
+
             base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseConfiguration());
+            modelBuilder.ApplyConfiguration(new ResourceConfiguration());
+            modelBuilder.ApplyConfiguration(new HomeworkConfiguration());
+            modelBuilder.ApplyConfiguration(new StudentCourseConfiguration());
+
+
             base.OnModelCreating(modelBuilder);
         }
     }
